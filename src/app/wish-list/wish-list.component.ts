42dccd142e5c4d6dc390901edfc0c241c79d0aa9
roman <roman.divonchuk@gmail.com>
2021-09-27 18:11:13 +0300

    todo list autocomp

--- src/app/wish-list/wish-list.component.ts
+++ src/app/wish-list/wish-list.component.ts
@@ -1,7 +1,9 @@
+import { FetchService } from './../fetch.service';
 
 import { Component, OnInit } from '@angular/core';
 import {FormControl} from '@angular/forms';
-
+import { Observable } from 'rxjs';
+import {map, startWith} from 'rxjs/operators';
 
 @Component({
   selector: 'app-wish-list',
@@ -9,20 +11,52 @@ import {FormControl} from '@angular/forms';
   styleUrls: ['./wish-list.component.scss']
 })
 export class WishListComponent implements OnInit  {
-  wishList: string[] = []
-  episode = new FormControl('');
+
+  myControl = new FormControl();
+  options: any[] = []
+  filteredOptions: Observable<any[]>;
+  wishList: any[] = []
+
+  constructor(private fetchService: FetchService) {}
   ngOnInit() {
-    this.episode.valueChanges.subscribe(value => {
-      console.log(value);
-    });
+    if (this.options.length === 0) {
+      this.fetchAllEpisodes()
+    }
+
+    this.filteredOptions = this.myControl.valueChanges
+      .pipe(
+        startWith(''),
+        map(str => str ? this._filter(str) : this.options.slice())
+      );
+  }
+
+  displayFn(str: any): string {
+    return str ? str : '';
+  }
+
+  private _filter(str: string): any[] {
+    const filterValue = str.toLowerCase();
+    return this.options.filter(option => option.name.toLowerCase().includes(filterValue));
   }
 
   addToList() {
-    if (this.episode.value) {
-      this.wishList.push(this.episode.value)
-      this.episode.reset()
+    if (this.myControl.value) {
+      let currEpisode = this.options.filter(item => item.name === this.myControl.value)
+      this.wishList.push(currEpisode[0])
+      this.myControl.reset()
     }
   }
+
+  fetchAllEpisodes(id=1) {
+    this.fetchService.getEpisodes(id).subscribe((value: any) => {
+      value.results.map((item:any) => this.options.push(item))
+      if (value.info.pages > id) {
+        this.fetchAllEpisodes(id+1)
+      }
+     })
+  }
+
+
 }
 
 
